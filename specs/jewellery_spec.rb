4f3b0e931047cd6c6f697e9ccaf3be39621d62dd
Jack Quigley <jackaquigley@gmail.com>
2020-04-05 21:34:23 +0100

    all jewellery tests now pass

--- specs/jewellery_spec.rb
+++ specs/jewellery_spec.rb
@@ -22,4 +22,24 @@ class JewelleryTest < Minitest::Test
     assert_equal('Titanium Dermal Anchor', @dermal.name)
   end
 
+  def test_jewellery_suitable_for_first
+  assert_equal(true, @dermal.suitable_for_first)
+  end
+
+  def test_jewellery_type
+  assert_equal("Dermal", @dermal.type)
+end
+
+def test_jewellery_price
+  assert_equal(40, @dermal.price)
+end
+
+def test_jewellery_size
+  assert_equal('14G', @dermal.size)
+end
+
+def test_jewellery_quantity
+  assert_equal(4, @dermal.quantity)
+end
+
 end
