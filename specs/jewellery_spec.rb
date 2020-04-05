f52ef91e70cc58c7d5e75c870a1987f28b3d73b3
Jack Quigley <jackaquigley@gmail.com>
2020-04-05 21:33:21 +0100

    tested for jewellery name and initialized jewellery class

---
+++ specs/jewellery_spec.rb
@@ -0,0 +1,25 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require('pry-byebug')
+
+require_relative('../models/jewellery')
+require_relative('../models/manufacturer')
+
+class JewelleryTest < Minitest::Test
+
+  def setup
+    @dermal = Jewellery.new({
+      'name' => "Titanium Dermal Anchor",
+      'suitable_for_first' => true,
+      'type' => 'Dermal',
+      'price' => 40,
+      'size' => '14G',
+      'quantity' => 4 })
+  end
+
+  def test_jewellery_name
+    assert_equal('Titanium Dermal Anchor', @dermal.name)
+  end
+
+end
